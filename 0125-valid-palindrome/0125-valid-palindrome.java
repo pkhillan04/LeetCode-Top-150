73f8f4c70a2dadd5abc7ca2067d904c87cbceefd
Parth Khillan <68949611+pkhillan04@users.noreply.github.com>
2024-10-16 20:50:27 -0400

    Time: 2 ms (99.18%), Space: 43.2 MB (60.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -5,22 +5,22 @@ class Solution {
         int j = s.length() - 1;
         
         while (i < j) {
-            // Skip non-alphanumeric characters from the front
+
             while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
                 i++;
             }
             
-            // Skip non-alphanumeric characters from the back
+
             while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
                 j--;
             }
             
-            // Compare the valid characters, convert to lowercase
+
             if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                 return false;
             }
             
-            // Move towards the middle
+
             i++;
             j--;
         }
